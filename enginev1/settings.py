7c789ef078c2110e2c7a92c73f4c32fea896e689
nsrivast <nsrivast@gmail.com>
2016-07-12 18:28:47 -0400

    whitenoise

--- enginev1/settings.py
+++ enginev1/settings.py
@@ -46,6 +46,7 @@ class Common(Configuration):
 
     MIDDLEWARE_CLASSES = [
         'django.middleware.security.SecurityMiddleware',
+        'whitenoise.middleware.WhiteNoiseMiddleware',
         'django.contrib.sessions.middleware.SessionMiddleware',
         'django.middleware.common.CommonMiddleware',
         'django.middleware.csrf.CsrfViewMiddleware',
@@ -137,8 +138,8 @@ class Common(Configuration):
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/1.9/howto/static-files/
     STATIC_URL = '/static/'
-
-    STATIC_ROOT = BASE_DIR + '/static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
     FIXTURE_DIRS = [
         'fixtures/',
