019d4a8856af450b2b288051793fb5649356a4f0
Jeff Chen <jeffrey@cmu.edu>
2016-07-31 15:27:09 -0700

    partial stuff

--- enginev1/static/scripts/main.js
+++ enginev1/static/scripts/main.js
@@ -23,55 +23,77 @@ $(document).ready(function(){
 
     var selectedItem;
 
-    var table = $('.dataset-table').DataTable({
-        paging:   true,
-        scrollX: true,
-        scrollY: true,
-        select: 'single',
-        lengthMenu: [ [10, 25, 250, -1], [10, 25, 250, 'All'] ],
-        buttons: [{
-            extend: 'colvis',
-            text: 'Select columns',
-        }],
-        columnDefs: [
-            { visible: false, targets: 0 }
-        ]
-    });
-    new $.fn.dataTable.Buttons(table, {
-        buttons: [{
-            text: 'View',
-            action: function(e, dt, node, config) {
-                // Do stuff
+    if ($('.dataset-table').length > 0) {
+        var table = $('.dataset-table').DataTable({
+            paging: true,
+            scrollX: true,
+            scrollY: true,
+            select: 'single',
+            lengthMenu: [[10, 25, 250, -1], [10, 25, 250, 'All']],
+            buttons: [{
+                extend: 'colvis',
+                text: 'Select columns',
+            }],
+            columnDefs: [
+                { visible: false, targets: 0 }
+            ]
+        });
+        new $.fn.dataTable.Buttons(table, {
+            buttons: [{
+                text: 'View',
+                action: function(e, dt, node, config) {
+                    // Do stuff
+                },
             },
-        },
-        {
-            text: 'Edit',
-            action: function(e, dt, node, config) {
-                // Do stuff
-            },
-        },
-        {
-            text: 'Delete',
-            action: function(e, dt, node, config) {
-                // Do stuff
-            },
-            className: 'btn-danger'
-        }]
-    });
-    table.buttons(1, null).disable();
-
-    $('.row>.col-sm-6:first-of-type', table.table().container()).prepend(table.buttons(0, null).container());
-    $('.row>.col-sm-6>.dt-buttons', table.table().container()).after(table.buttons(1, null).container());
-
-    table.on('select', function(e, dt, type, indexes) {
-        selectedItem = $(table[type](indexes).nodes().to$()[0]);
-        table.buttons(1, null).enable();
-    });
-
-    table.on('deselect', function(e, dt, type, indexes) {
-        selectedItem = null;
+                {
+                    text: 'Edit',
+                    action: function(e, dt, node, config) {
+                        // Do stuff
+                    },
+                },
+                {
+                    text: 'Delete',
+                    action: function(e, dt, node, config) {
+                        // Do stuff
+                    },
+                    className: 'btn-danger'
+                }]
+        });
         table.buttons(1, null).disable();
+
+        $('.row>.col-sm-6:first-of-type', table.table().container()).prepend(table.buttons(0, null).container());
+        $('.row>.col-sm-6>.dt-buttons', table.table().container()).after(table.buttons(1, null).container());
+
+        table.on('select', function(e, dt, type, indexes) {
+            selectedItem = $(table[type](indexes).nodes().to$()[0]);
+            table.buttons(1, null).enable();
+        });
+
+        table.on('deselect', function(e, dt, type, indexes) {
+            selectedItem = null;
+            table.buttons(1, null).disable();
+        });
+    }
+
+    $('.radio-inline')[0].click();
+
+    $('#custom-search').click(function(event) {
+        $('#radio-custom').prop('checked', true);
     });
 
+    $('#newcustom').click(function(event) {
+        $('#radio-newcustom').prop('checked', true);
+    });
 
+    $('#radio-custom, #radio-newcustom').on('change', function(event) {
+        console.log(this.checked);
+        console.log(this.value);
+        if (this.checked && this.value.indexOf('custom') !== -1)
+            showCustomSearch();
+    });
+
+    function showCustomSearch() {
+        $('input[name="save-custom-search"]').toggleClass('hidden');
+        $('.custom-search-options').toggleClass('hidden');
+    }
 });
