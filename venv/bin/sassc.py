ad138e581834e2cd83fb1d1cfb00d7e23c5bdf5b
nsrivast <nsrivast@gmail.com>
2016-07-31 22:24:04 -0400

    ns temp on jeff/SCSS

--- venv/bin/sassc.py
+++ venv/bin/sassc.py
@@ -1,4 +1,4 @@
-#!/Users/jeff/Documents/twine/enginev1/venv/bin/python
+#!/Users/ns/Dropbox/twine/Product/enginev1/venv/bin/python
 """:mod:`sassc` --- SassC compliant command line interface
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
