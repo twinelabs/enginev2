01eb26c6cda7ad6f01aa6b06765c207b145d0e08
nsrivast <nsrivast@gmail.com>
2016-09-28 21:16:00 -0400

    view results bug

--- enginev1/apps/match/utils.py
+++ enginev1/apps/match/utils.py
@@ -74,9 +74,9 @@ def match_results_as_html(match):
     elif cfg['task'] == 'assign':
         n_lim = 4
 
-        dt_B = match.data_tables.all()[1]
+        dt_B = match.data_tables.all()[0]
         dt_B_values = dt_B.values()
-        dt_A = match.data_tables.all()[0]
+        dt_A = match.data_tables.all()[1]
         dt_A_values = dt_A.values()
 
         header = dt_B.header()[:n_lim] + [""] + dt_A.header()[:n_lim]
